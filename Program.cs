7dd64049149ace272150cc85c4d7077e4b533bc7
Adailton Oliveira <adailtonscout@yahoo.com.br>
2023-01-31 13:08:46 -0300

    Solução do desafio de código Criando um Sistema e Abstraindo um Celular com POO em C#

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+var nokia = new Nokia("999999999", "Galaxy S22 Ultra", "123456789", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Dio Voice");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Iphone:");
+var Iphone = new Iphone("888888888", "Galaxy S20 FE ", "987654321", 128);
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Dio VideoChamada");
